6f851d98c3ebad6489320b15fe92e84f91493812
Keich.Park <svclaw2000@gmail.com>
2022-01-11 02:47:03 +0900

    feat: HWA-7 Presentation Layer 구성

--- presentation/src/main/java/com/khnsoft/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/khnsoft/presentation/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.khnsoft.homeworkalarm
+package com.khnsoft.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khnsoft.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
